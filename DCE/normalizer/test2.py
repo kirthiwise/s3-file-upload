bfe6b244fa774d56edc272bc7131097b6bb110c3
kirthiwise <kprabakaran@streamwisely.com>
2022-09-21 12:42:09 +0530

    asdfierw

--- DCE/normalizer/test2.py
+++ DCE/normalizer/test2.py
@@ -1,6 +1,1 @@
-print("This is my world...")
-print("This is my second world")
-sadfkjldsf
-asfsdf
-print("Hey This is my Final Words...")
-print("HeyEveryone .. lets start the meeting..")
+print("oloioopl")
