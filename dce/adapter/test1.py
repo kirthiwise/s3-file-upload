a4bd957b1253e2bb42681511bca0b44adf4fe54d
kirthiwise <kprabakaran@streamwisely.com>
2022-09-28 12:06:33 +0530

    ksouiew

--- dce/adapter/test1.py
+++ dce/adapter/test1.py
@@ -1,6 +1,2 @@
-1
-2
-3sadfasfsadfsdf
-4
-5
-6
+sdafsadfkjkldsaf
+sadkjf;klj
--- dce/normalizer/test2.py
+++ dce/normalizer/test2.py
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-sadkfjldkasfj
-asdfhkjdsaf
-ksadhf
-asdhjkhiuewhkjdsf
-jksafdlksdaf
-kjashfdkj
-=======
-sadkfjasdf
-sdaklfjklsdaj
-sdasdfsd
->>>>>>> 9d45dbf9c5311fc54232d0632e019fe146942000
+"sdfkljlskdfjlksdflk"
