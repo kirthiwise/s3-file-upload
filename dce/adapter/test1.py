d28603e90bccaec61c235ee68fa5190b848509a3
kirthiwise <kprabakaran@streamwisely.com>
2022-09-28 13:32:21 +0530

    sdfsdf

--- dce/adapter/test1.py
+++ dce/adapter/test1.py
@@ -1,10 +1,6 @@
-sdafsadfkjkldsaf
-sadkjf;klj
 
-sadkfjlksdfj
-asdfas
-asdfa
-dasfdsafsafds
-dsaf
-asdfdfssdfsdf
-sadfjkljasdfsdafkjsdf
+always() &&
+      needs.detect-file-changes.outputs.datalake == 'true' &&
+      github.event.push == true &&
+      ((needs.deploy-frontend.result == 'success' || needs.deploy-frontend.result == 'skipped') ||
+      (needs.deploy-backend.result == 'success' || needs.deploy-backend.result == 'skipped'))
